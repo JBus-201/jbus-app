bdbbb1931e97cf75b16721a3bf559e7b040c3a99
KhaderAbumallouh <kiabumallouh20@cit.just.edu.jo>
2023-11-28 21:21:02 +0300

    firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:jbus_app/screens/authentication/signup/signup.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'generated/l10n.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:jbus_app/constants/firebase_options.dart';
 
 Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Future.delayed(const Duration(seconds: 4));
   FlutterNativeSplash.remove();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
