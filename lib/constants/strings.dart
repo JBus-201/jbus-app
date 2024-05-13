a14b3e5cdc53992c782630ed2481007b68791252
AsimQarawi <asimqarawy@gmail.com>
2024-05-13 16:00:31 +0300

    ز

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -1,2 +1,2 @@
 const String googlApiKey = 'AIzaSyBGSGYjRajQX1-0LksuNshClN1l8ORObJA';
-const String baseUrl = 'http://vmi1560602.contaboserver.net/api/v1.0';
+const String baseUrl = 'http://5.252.55.246/api/v1.0';
